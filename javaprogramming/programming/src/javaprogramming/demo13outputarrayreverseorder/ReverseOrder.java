e50f780d0e4a7222ea205e7844923d5c5380d004
LTL <c language_test@it.cn>
2025-11-04 21:46:35 +0800

    2025-11-4第二次提交

--- javaprogramming/programming/src/javaprogramming/demo13outputarrayreverseorder/ReverseOrder.java
+++ javaprogramming/programming/src/javaprogramming/demo13outputarrayreverseorder/ReverseOrder.java
@@ -2,7 +2,20 @@ package javaprogramming.demo13outputarrayreverseorder;
 
 public class ReverseOrder {
     public static void ArrayReverseOrder(int[] array){
-        for (int i = array.length - 1; i >= 0; i--) {
+        // 使用冒泡排序实现数组从大到小排序
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                // 如果前面的元素小于后面的元素，则交换位置
+                if (array[j] < array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        // 输出排序后的数组
+        for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + "\t");
         }
     }
--- javaprogramming/programming/src/javaprogramming/demo13outputarrayreverseorder/Test.java
+++ javaprogramming/programming/src/javaprogramming/demo13outputarrayreverseorder/Test.java
@@ -1,14 +1,19 @@
 package javaprogramming.demo13outputarrayreverseorder;
 
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
         //目标：编写一个程序，逆序输出一个整型数组中的元素。
-        int[] arr = {1,2,3,4,5,6,7,8,9,10};
-        System.out.println("顺序：");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + "\t");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入数组大小：");
+        int length = sc.nextInt();
+        System.out.println("请输入数组：");
+        int[] arr = new int[length];
+        for (int i = 0; i < length; i++) {
+            arr[i] = sc.nextInt();
         }
-        System.out.println("\n逆序：");
+        System.out.println("逆序：");
         ReverseOrder.ArrayReverseOrder(arr);
     }
 }
